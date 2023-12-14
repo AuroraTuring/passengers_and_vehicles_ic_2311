cdec10be0b83a57b42fffa6b72292a492dd8d439
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-14 14:27:48 -0700

    Initial commit.

---
+++ spec/park_spec.rb
@@ -0,0 +1,18 @@
+require "./lib/passenger"
+require "./lib/vehicle"
+require "./lib/park"
+
+
+
+RSpec.describe Park do
+  it "exists" do 
+    text
+  end
+
+  it "has attributes" do 
+    text
+  end
+
+
+
+end
