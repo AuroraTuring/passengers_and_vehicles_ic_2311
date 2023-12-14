d241d6a36eed23cbbf7647d07947c24dc237b427
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-14 14:59:39 -0700

    Set up test templates.

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -6,12 +6,27 @@ require "./lib/park"
 
 RSpec.describe Park do
   it "exists" do 
-    text
+    park = Park.new ("RMNP", 25)
+
+    expect(park).to be_an_instance_of(Park)
   end
 
   it "has attributes" do 
-    text
+    park = Park.new ("RMNP", 25)
+
+    expect(park.name).to eq("RMNP")
+    expect(park.admission_price).to eq(25)
   end
+
+  it "can add vehicles" do
+    
+  end
+
+  it "can calculate revenue" do
+
+  end
+
+
 
 
 
