8acd27f3cbda6fcb205cef61c7dd42ed295f774d
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-14 13:59:51 -0700

    Fill in incomplete methods.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,20 +1,25 @@
 class Vehicle
-  attr_reader :year, :make, :model
+  attr_reader :year, :make, :model, :passengers
 
   def initialize(year, make, model)
     @year = year
     @make = make 
     @model = model 
+    @speeding = false 
     @passengers = []
   end
 
-  # def speeding?
-  #   method 
-  # end
+  def speeding?
+    @speeding
+  end
 
-  # def add_passenger
-  #   @passengers << :passenger
-  # end 
+  def speed 
+    @speeding = true
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end 
 
   # def num_adults
   #   method
